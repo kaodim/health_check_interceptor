a88ecce4d3a94d1c14a8d710630a8e3f991050cc
khun84 <khun84@gmail.com>
2019-08-15 23:50:41 +0800

    Remove sample spec

--- spec/health_check_interceptor_spec.rb
+++ spec/health_check_interceptor_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe HealthCheckInterceptor do
   it "has a version number" do
     expect(HealthCheckInterceptor::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
